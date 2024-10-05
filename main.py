7bd87d5b3d175923f21972518cd6f88132e76d75
GSSonoda <gustavoshoitisonoda@hotmail.com>
2024-10-05 19:38:55 +0000

    update dry-analisis API

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from flask import Flask, request, jsonify
-from cultura.enum import AltaSusceptibilidade, MediaSusceptibilidade, BaixaSusceptibilidade
+from cultura.enum import MuitoAltaSusceptibilidade, AltaSusceptibilidade, MediaSusceptibilidade, BaixaSusceptibilidade, MuitoBaixaSusceptibilidade
+from google_earth_engine.init_engine import GoogleEartnInterface
+
 
 app = Flask(__name__)
+google = GoogleEartnInterface()
+
+
+def get_susceptibility(culture):
+    if culture in MuitoAltaSusceptibilidade.__members__.keys():
+        return VERY_HIGH
+    if culture in AltaSusceptibilidade.__members__.keys():
+        return HIGH
+    if culture in MediaSusceptibilidade.__members__.keys():
+        return MEDIUM
+    if culture in BaixaSusceptibilidade.__members__.keys():
+        return LOW
+    if culture in MuitoBaixaSusceptibilidade.__members__.keys():
+        return VERY_LOW
 
 @app.route("/dry-analysis", methods=["POST"])
 def dry_analysis():
@@ -9,20 +25,28 @@ def dry_analysis():
         "culture": request.json.get('culture'),
         "latitude": request.json.get('latitude'),
         "longitude": request.json.get('longitude'),
-        "radio": request.json.get('radio'),                         # raio em kilometros do circulo
+        "radio_km": request.json.get('radio_km'),                   # raio em kilometros do circulo
         "irrigation": request.json.get('irrigation'),               # "Tem irrigação ou capacidade de investir em uma?"
         "planting_period": request.json.get('planting_period'),     # "Estamos antes, durante ou depois o período ideal de plantio?"
         "existing_culture": request.json.get('existing_culture')    # "Já tem cultura plantada no momento?"
     }
-    # Prepare a resposta
+    susceptibility = get_susceptibility(data_input['culture'])
     output = {
-        "susceptibility": 0,    # Nota da Suscetibilidade => 1 ALTA | 0.66 MEDIA | 0.33 BAIXA
-        "wbi_medium": 0,        # Balanço Hídrico
-        "ndmi_medium": 0,       # UMIDADE
-        "sb": 0,                # SB [ALTO; MEDIO E BAIXO ]
-
+        "susceptibility": susceptibility,   # Nota da Suscetibilidade da cultura => 1 ALTA | 0.66 MEDIA | 0.33 BAIXA
+        "wbi_medium": 0,                    # Balanço Hídrico
+        "sb": 0,                            # SB Balanço Hídrico
+        "ndmi_medium": 0,                   # UMIDADE
+        "su": 0                             # SU Umidade
     }
     return jsonify(output)
 
 if __name__ == "__main__":
+    VERY_HIGH = 0.9
+    HIGH = 0.7
+    MEDIUM = 0.5
+    LOW = 0.3
+    VERY_LOW = 0.1
+
+    
+    
     app.run(host="0.0.0.0", port=8000, debug=True)
