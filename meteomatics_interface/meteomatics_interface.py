4bcf468466807ceefcfae62c8c68568d87b9caaf
GSSonoda <gustavoshoitisonoda@hotmail.com>
2024-10-06 21:12:08 +0000

    update meteomatics interface

--- meteomatics_interface/meteomatics_interface.py
+++ meteomatics_interface/meteomatics_interface.py
@@ -2,7 +2,7 @@ import requests
 import base64
 import datetime
 
-class MeteomaticsAPI:
+class MeteomaticsInterface:
     def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
@@ -10,7 +10,7 @@ class MeteomaticsAPI:
         self.api_url = 'https://api.meteomatics.com'
         self.token = None
 
-    def get_token(self):
+    def _get_token(self):
         credentials = f"{self.username}:{self.password}".encode("utf-8")
         auth_header = base64.b64encode(credentials).decode("utf-8")
 
@@ -26,9 +26,9 @@ class MeteomaticsAPI:
         else:
             raise Exception(f"Erro ao obter o token: {response.status_code} - {response.text}")
 
-    def get_precipitation(self, latitude: float, longitude: float, date_time: datetime.datetime):
+    def get_precipitation_one_days(self, latitude: float, longitude: float, date_time: datetime.datetime):
         if not self.token:
-            self.get_token()
+            self._get_token()
 
         formatted_time = date_time.strftime('%Y-%m-%dT%H:%M:%SZ')
         weather_url = f"{self.api_url}/{formatted_time}/precip_24h:mm/{latitude},{longitude}/json"
@@ -46,18 +46,40 @@ class MeteomaticsAPI:
         else:
             raise Exception(f"Erro ao obter os dados do tempo: {response.status_code} - {response.text}")
 
+    def get_precipitation_days(self, latitude: float, longitude: float, start_datetime: datetime.datetime,  end_datetime: datetime.datetime):
+        if not self.token:
+            self._get_token()
+
+        start_datetime = start.strftime('%Y-%m-%dT%H:%M:%SZ')
+        end_datetime = start.strftime('%Y-%m-%dT%H:%M:%SZ')
+        url = f"{self.api_url}/{start_datetime}--{end_datetime}:P1D/precip_24h:mm/{latitude},{longitude}/json?model=mix"
+
+        # Faz a requisição da previsão do tempo
+        response = requests.get(
+            url,
+            headers={
+                'Authorization': f'Bearer {self.token}'
+            }
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Erro ao obter os dados do tempo: {response.status_code} - {response.text}")
+
 if __name__ == "__main__":
     username = "sonoda_gustavoshoiti"
     password = "5P6Kmg1ktI"
     
-    api = MeteomaticsAPI(username, password)
+    api = MeteomaticsInterface(username, password)
     
     latitude = 52.520551
     longitude = 13.461804
-    date_time = datetime.datetime.now()
+    start = datetime.datetime.now()
+    end = start + datetime.timedelta(days=14)
 
     try:
-        weather_data = api.get_precipitation(latitude, longitude, date_time)
+        weather_data = api.get_precipitation_days(latitude, longitude, start, end)
         print(weather_data)
     except Exception as e:
         print(str(e))
